5b6318155b8641858ebdacb7b6042ed8d6e7b444
Ayub <ayub@email.com>
2026-05-05 00:29:12 +0200

    used while loop to keep listening for the command word

--- main.py
+++ main.py
@@ -8,5 +8,21 @@ engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
 if __name__ == "__main__":
     speak("initializing Jarvis")
+    while True:
+        # Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+
+        # recognize speech using Sphinx
+        try:
+          print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+          print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+          print("Sphinx error; {0}".format(e))
